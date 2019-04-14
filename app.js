bd51ba637cab6cfef28722035304f776cd24550d
Anita Virag <ik.virag@gmail.com>
2019-04-14 12:20:04 +0200

    components

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+var data = {
+  name: 'Yoshi'
+};
+
+Vue.component('greeting', {
+  template: '<p>Hey there, I am {{ name }} <button v-on:click="changeName">Change name</button></p>',
+  data: function() {
+    // return data;
+    return {
+      name: 'Anita'
+    };
+  },
+  methods: {
+    changeName: function() {
+      this.name = 'Mario';
+    }
+  }
+});
+
+
 var one = new Vue({
   el: '#vue-app-one',
   data: {
